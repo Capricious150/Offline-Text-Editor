ae061cae1fec74a3322805b36d89f226420555c5
Austin A <austinandrews89@gmail.com>
2022-04-10 15:23:45 -0600

    Began writing install functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,4 +8,7 @@ window.addEventListener('beforeinstallprompt', (event) => {});
 butInstall.addEventListener('click', async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    alert("Looks like the install was successful, ain't that something?")
+    console.log('Good!', 'appinstalled', event);
+  });
